736437edd8b05da5c9acdbec9a2ec176cfe4ce5f
rachelhartanto <32819027+rachelhartanto@users.noreply.github.com>
2017-11-04 15:14:23 -0400

    Wwise171004.01
    Wwise171004.01

---
+++ Better Perception_WwiseIntegrationTemp/Assets/Scripts/FootSteps.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+
+
+    //    [RequireComponent(typeof(AudioSource))]
+    //    [RequireComponent(typeof(CharacterController))]
+
+    private CharacterController _controller;
+
+    bool isPlaying;
+
+
+
+    void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Use this for initialization
+
+
+    void Update()
+    {
+
+        //        RaycastHit hit;
+        //        Ray landingRay = new Ray(transform.position, Vector3.down);
+
+        //        if (Physics.Raycast (landingRay, out hit, 1f)) {
+        //
+        //
+        //            if (hit.collider.tag == "Terrain") {
+        //
+        //                Debug.Log ("Terrain");
+        //            } 
+        //            else if (hit.collider.tag == "Concrete") {
+        //
+        //                Debug.Log ("Concrete");
+        //            } 
+        //            else if (hit.collider.tag == "Wood") {
+        //
+        //                Debug.Log ("Wood");
+        //            }
+        //            else if (hit.collider.tag == "Tile") {
+        //
+        //                Debug.Log ("Tile");
+        //            }
+        //            else if (hit.collider.tag == "BathroomTile") {
+        //
+        //                Debug.Log ("BathroomTile");
+        //            }
+        //            else if (hit.collider.tag == "SoftTile") {
+        //
+        //                Debug.Log ("SoftTile");
+        //            }
+        //        }
+
+
+
+        if (Input.GetAxis("Horizontal") > 0.1f && isPlaying == false)
+        {
+            StartCoroutine("FootStep");
+        }
+
+
+        if (Input.GetAxis("Horizontal") < -0.1f && isPlaying == false)
+        {
+            StartCoroutine("FootStep");
+        }
+
+
+        if (Input.GetAxis("Vertical") > 0.1f && isPlaying == false)
+        {
+            StartCoroutine("FootStep");
+        }
+
+
+        if (Input.GetAxis("Vertical") < -0.1f && isPlaying == false)
+        {
+            StartCoroutine("FootStep");
+
+        }
+
+    }
+
+
+
+
+
+
+    IEnumerator FootStep()
+    {
+
+        isPlaying = true;
+
+
+        RaycastHit hit = new RaycastHit();
+
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            tag = hit.collider.gameObject.tag;
+            if (tag == "Terrain")
+            {
+                AkSoundEngine.SetSwitch("Footsteps", "Terrain", gameObject);
+            }
+            else if (tag == "Concrete")
+            {
+                AkSoundEngine.SetSwitch("Footsteps", "Concrete", gameObject);
+            }
+            else if (tag == "Wood")
+            {
+                AkSoundEngine.SetSwitch("Footsteps", "Wood", gameObject);
+            }
+            else if (tag == "Tile")
+            {
+                AkSoundEngine.SetSwitch("Footsteps", "Tile", gameObject);
+            }
+            else if (tag == "BathroomTile")
+            {
+                AkSoundEngine.SetSwitch("Footsteps", "BathroomTile", gameObject);
+            }
+            else if (tag == "SoftTile")
+            {
+                AkSoundEngine.SetSwitch("Footsteps", "SoftTile", gameObject);
+            }
+        }
+
+        AkSoundEngine.PostEvent("Footsteps", gameObject);
+
+        yield return new WaitForSeconds(0.5f);
+
+
+        isPlaying = false;
+        //                }
+        //                else {
+        //                    yield return 0;
+        //                }
+        //            }
+        //        AkSoundEngine.PostEvent ("Footsteps", gameObject);
+
+    }
+
+    private void PlayFootStepAudio()
+    {
+        if (_controller.isGrounded)
+        {
+            return;
+        }
+    }
+}
---
+++ Better Perception_WwiseIntegrationTemp/Library/Collab/Download/Assets/Sonar/WorkingScanner/rippleBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Change rippleBehavior to be an event listener that takes in 
+ * ripple source location (x,y,z) 
+ * Everytime we hit the cain against something, we emit an event 
+ * 
+ * TODO: Set cutoff distances where the ripples stop
+ * TODO: 
+ */
+
+
+//[ExecuteInEditMode]
+public class rippleBehavior: MonoBehaviour {
+	public Transform origin;
+	public Renderer rend;
+	float radius = 0.0f;
+	Material mat;
+	float upperBound = 10.0f;
+	public Color wireColor;
+	public Color baseColor;
+	public float thickness = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		rend.material.shader = Shader.Find("Custom/Echolocation");
+		Vector3 origin_position = new Vector3 (origin.position.x, origin.position.y, origin.position.z);
+		rend.material.SetVector ("_Center",origin_position );
+		rend.material.SetColor ("_WireColor", wireColor);
+		rend.material.SetColor ("_BaseColor", baseColor);
+		rend.material.SetFloat ("_WireThickness", thickness);
+	}
+
+	void Update () {
+		Debug.Log ("-----------");
+		/*
+		if (radius < upperBound) {
+			radius += 0.05f;
+			//thickness = 0.0f;
+		} else {
+
+			radius = 0.0f;
+			thickness = 10.0f;
+		}
+		*/
+		rend.material.SetFloat ("_Radius", radius);
+		rend.material.SetFloat ("_WireThickness", thickness);
+
+	}
+}
